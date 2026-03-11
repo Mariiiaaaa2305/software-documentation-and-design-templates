0b627c388deecbdd2986ba50070bc3b9a0dbf0e5
Mariiiaaaa2305 <tsiupka46@gmail.com>
2026-03-11 22:05:24 +0200

    Update IUserRepository.cs

--- MyLab.DataAccessLayer/IUserRepository.cs
+++ MyLab.DataAccessLayer/IUserRepository.cs
@@ -4,9 +4,8 @@ namespace MyLab.DataAccessLayer;
 
 public interface IUserRepository
 {
-    // Отримати всіх користувачів із CSV файлу
+
     List<User> ReadUsersFromCsv(string filePath);
-    
-    // Зберегти список користувачів у базу даних
+
     void SaveUsersToDatabase(List<User> users);
 }
